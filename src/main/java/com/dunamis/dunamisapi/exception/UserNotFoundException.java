e0a66dcbfada2d82613a65baa406683be56160bf
Justin <cruzjustin1428@gmail.com>
2024-08-02 11:09:03 -0600

    CRUD Usuarios Funcional

--- src/main/java/com/dunamis/dunamisapi/exception/UserNotFoundException.java
+++ src/main/java/com/dunamis/dunamisapi/exception/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package com.dunamis.dunamisapi.exception;
 
 public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id){
+    public UserNotFoundException(Integer id){
         super("Could not found the user with id "+ id);
     }
 }
